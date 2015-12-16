780254b5ad9d0ba0e688d0a14457f5ae1f42cac9
James Larisch <jamesfordummies@gmail.com>
2015-12-16 16:49:02 -0500

    works

--- src/clojure_chores/core.clj
+++ src/clojure_chores/core.clj
@@ -3,18 +3,15 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
-(def TENANTS '(:james :vivian :someone :kevin :jake))
-(def CHORES '(:fridge :kitchen :downstairs :upstairs :trash))
 (def FILE (System/getenv "CHORES_FILE"))
 
 (defn now [] (java.util.Date.))
 
-(defn rotate
-  [chores]
-  (concat (rest chores) (list (first chores))))
+(defn rotate [chores]
+  (conj (vec (rest chores)) (first chores)))
 
 (defn -main
-  "Some stuff..."
+  "Rotates chores for the week."
   [& Args]
   (let [raw (slurp FILE)
         split_up (doall (str/split raw #"\n"))
@@ -22,7 +19,8 @@
         people (second split_up)
         chores (nth split_up 2)
         split_people (str/split people #" ")
-        split_chores (str/split chores #" ")]
-    (cond (= "rotate" (first Args)) (spit FILE (doall (str/join "\n" (list (now) people (rotate chores)))))
+        split_chores (str/split chores #" ")
+        ]
+    (cond (= "rotate" (first Args)) (spit FILE (str/join "\n" (list (now) people (str/join " " (rotate split_chores)))))
           (= "show" (first Args)) (println (map vector split_people split_chores))
           :else (println "Command must be either rotate or show."))))
